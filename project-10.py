d43aa43ef4ef9ea44f0a9e74d4b2e4e42e0b50c0
Dzarel Developer <112155072+DzarelDeveloper@users.noreply.github.com>
2024-12-19 22:00:33 +0700

    Add files via upload

---
+++ project-10.py
@@ -0,0 +1,56 @@
+# Project-10 : AI CHATGPT API
+# Codesphered01010
+
+import requests
+import pyfiglet
+import itertools
+import threading
+import time
+import sys
+
+url = "https://simple-chatgpt-api.p.rapidapi.com/ask"
+
+
+headers = {
+    "content-type": "application/json",
+    "X-RapidAPI-Key": "YOUR-API-KEY",
+    "X-RapidAPI-Host": "simple-chatgpt-api.p.rapidapi.com"
+}
+
+
+def animate():
+    for c in itertools.cycle(['|', '/', '-', '\\']):
+        if done:
+            break
+        sys.stdout.write('\r' + c)
+        sys.stdout.flush()
+        time.sleep(0.1)
+
+    sys.stdout.write('\r')
+    sys.stdout.flush()
+
+
+def ask(question):
+    payload = {"question": question}
+    response = requests.post(url, json=payload, headers=headers)
+    return response.json().get("answer")
+
+
+if __name__ == "__main__":
+    print(pyfiglet.figlet_format("ChatGPT"))
+    print("Enter the question to ask:")
+    print()
+    while True:
+        question = str(input(">>  "))
+        if (question == 'q'):
+            print(">>  Bye! Thanks for Using...")
+            break
+        done = False
+        t = threading.Thread(target=animate)
+        t.start()
+        answer = ask(question)
+        time.sleep(5)
+        done = True
+        t.join()
+        print(">> ", answer)
+        print()
